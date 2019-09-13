f965240a47a310c710c6d173866b9973cc8de3a2
root <root@localhost.localdomain>
2019-09-13 14:58:40 +0200

    v1

---
+++ clients.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projet";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT idclient, Nom, datedenaissance FROM client";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo " - le id " . $row["idclient"]. " - le nom: " . $row["Nom"]. " - la date de naissance " . $row["datedenaissance"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
