c9efe65bb4f6580cd43487cca285d839935b99b2
stagiaire <stagiaire@localhost.localdomain>
2019-09-18 15:17:45 +0200

    only for linux

--- addclient.php
+++ addclient.php
@@ -2,7 +2,7 @@
 $nom = $_POST['nom'];
 $datedenaissance = $_POST['datedenaissance'];
 $numerodecompte = $_POST['numerodecompte'];
-$link = mysqli_connect("localhost","root","","projet");
+$link = mysqli_connect("172.17.0.2","root","","projet");
 mysqli_query($link,"insert into client values (0,\"".$nom."\",\"".$datedenaissance."\",\"".$numerodecompte."\")");
 $sql="insert into client values (0,\"".$nom."\",\"".$datedenaissance."\",\"".$numerodecompte."\")";
 echo $sql;
